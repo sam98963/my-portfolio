acb200ebb0f2e8dd780e489ba7145088d920d3c0
sam98963 <szwilliamson01@gmail.com>
2023-09-16 16:30:50 +0100

    header and some simple flexbox

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#22223B",
+        secondary: "#FDF2F0",
+        tertiary: "#F1F1F1"
+
+      }
+    },
   },
   plugins: [],
 }
