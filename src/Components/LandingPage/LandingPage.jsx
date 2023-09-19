616e57259abf107a97143b057e33480831fe23df
sam98963 <szwilliamson01@gmail.com>
2023-09-19 16:34:38 +0100

    full screen landing page?

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,9 +1,9 @@
 import Header from "../Header/Header"
 export default function LandingPage(){
   return(
-    <main className="lg:h-screen h-auto w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
+    <main className="h-screen w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
       <Header />
-      <section className="sm:flex-grow h-3/5 w-full flex sm:items-end items-center sm:p-15 p-10 pt-44 sm:pt-56 lg:pt-0">
+      <section className="flex-grow w-full flex items-end text-start sm:p-15 p-10 pt-44 sm:pt-56 lg:pt-0">
         <h1 className="sm:text-8xl md:text-9xl text-6xl">
           A Portfolio that matters.
         </h1>
@@ -11,3 +11,19 @@ export default function LandingPage(){
     </main>
   )
 }
+
+
+
+// import Header from "../Header/Header"
+// export default function LandingPage(){
+//   return(
+//     <main className="lg:h-screen h-auto w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
+//       <Header />
+//       <section className="sm:flex-grow h-3/5 w-full flex sm:items-end items-center text-center sm:text-start sm:p-15 p-10 pt-44 sm:pt-56 lg:pt-0">
+//         <h1 className="sm:text-8xl md:text-9xl text-6xl">
+//           A Portfolio that matters.
+//         </h1>
+//       </section>
+//     </main>
+//   )
+// }
