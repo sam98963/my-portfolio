e2e8cb213dee3fc6ef82b473f7ca18465305a216
sam98963 <szwilliamson01@gmail.com>
2023-09-23 17:22:56 +0100

    Change to landing page content, also slight change in responsiveness

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -3,10 +3,13 @@ export default function LandingPage(){
   return(
     <main className="h-screen w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
       <Header />
-      <section className="flex-grow w-full flex items-end text-start sm:p-15 p-10 pb-20 md:pb-10 lg:pt-0">
-        <h1 className="sm:text-8xl md:text-9xl text-6xl">
-          A Portfolio that matters.
-        </h1>
+      <section className="flex-grow w-full flex items-end text-start sm:p-15 p-5 sm:p-10  md:pb-10 lg:pt-0">
+        <article>
+          <h3 className="sm:text-4xl md:text-3xl text-2xl w-4/5">Musician, Triathlete and <span className="underline underline-offset-6 text-tertiary">Developer.</span></h3>
+          <h1 className="sm:text-7xl md:text-9xl text-4xl w-full">
+            Samuel Williamson
+          </h1>
+        </article>
       </section>
     </main>
   )
