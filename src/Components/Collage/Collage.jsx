f9e99c37e40c04250782610641eb4572d62475cf
sam98963 <szwilliamson01@gmail.com>
2023-09-29 18:49:45 +0100

    Modal changing details based on image hover

--- src/Components/Collage/Collage.jsx
+++ src/Components/Collage/Collage.jsx
@@ -6,9 +6,39 @@ import ProjectsModal from "../../Components/ProjectsModal/ProjectsModal"
 import { useState } from "react"
 export default function Collage(){
   const [modalHidden, setModalHidden] = useState("hidden")
-
-  function handleOnHover(){
+  const [projectDetails, setProjectDetails] = useState({})
+  function handleOnHover(projectKey){
+    switch(projectKey) {
+      case "Lyra":
+        setProjectDetails({
+          title: "Lyra Strings",
+          siteLink: "Lyra Site",
+          repoLink: "Lyra Repo"
+        });
+        break;
+      case "Portfolio":
+        setProjectDetails({
+          title: "This Portfolio",
+          repoLink: "Portfolio Link"
+        });
+        break;
+      case "Three":
+        setProjectDetails({
+          title: "Three Login Page Redesign",
+          siteLink: "Three Redesign Site",
+          repoLink: "Three Redesign Repo"
+        });
+        break;
+      case "MMM":
+        setProjectDetails({
+          title: "Mighty Mini Minds",
+          siteLink: "Mighty Mini Minds Site",
+          repoLink: "Mighty Mini Minds Repo"
+        });
+        break;
+    }
     setModalHidden("block")
+    console.log(projectDetails)
   }
   function handleOffHover(){
     setModalHidden("hidden")
@@ -17,17 +47,17 @@ export default function Collage(){
   <main  className="w-screen h-auto lg:h-screen flex items-center justify-center bg-gradient-to-br from-primary via-forth to-tertiary" >
       <section className="w-4/5 h-4/5 overflow-hidden rounded-2xl relative">
         <figure className="h-full w-full rotate-12 scale-150 cursor-pointer ">
-       <ProjectsModal projectdetails = "" visibility = {modalHidden} handleOffHover = {handleOffHover}/>
+       <ProjectsModal projectDetails = {projectDetails} visibility = {modalHidden} handleOffHover = {handleOffHover}/>
          <div className="h-1/2 lg:w-full w-1/2 flex">
-           <img src={Lyra} onMouseEnter = {handleOnHover} 
+           <img src={Lyra} onMouseEnter = {()=>handleOnHover("Lyra")}
              className="flex-2 h-full w-full object-cover lg:p-2 sm:p-1.5 p-1 rounded-lg"/>
-           <img src={PortfolioImg} 
+           <img src={PortfolioImg} onMouseEnter = {()=>handleOnHover("Portfolio")}
            className="flex-1 h-full w-full object-cover lg:p-2 sm:p-1.5 p-1 rounded-lg"/>
          </div>
          <div className="h-1/2 lg:w-full w-1/2 flex">
-         <img src={ThreeImg}
+         <img src={ThreeImg} onMouseEnter = {()=>handleOnHover("Three")}
            className="flex-1 h-full w-full object-cover lg:p-2 sm:p-1.5 p-1"/>
-           <img src={MightyMiniMindsImg}
+           <img src={MightyMiniMindsImg} onMouseEnter = {()=>handleOnHover("MMM")}
            className="flex-2 h-full w-full object-cover lg:p-2 sm:p-1.5 p-1"/>
          </div>
        </figure>
--- src/Components/ProjectsModal/ProjectsModal.jsx
+++ src/Components/ProjectsModal/ProjectsModal.jsx
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types"
-export default function ProjectsModal({visibility, handleOffHover}){
+export default function ProjectsModal({visibility, handleOffHover, projectDetails}){
     let modalClass = `fixed inset-0 flex items-center justify-center bg-red-500 w-full h-full z-20 ${visibility}`
   return(
     <section onMouseLeave={handleOffHover} className={modalClass}>
-      <h1>Mighty Mini Minds</h1>
-      <p>Link to repo</p>
-      <p>Link to site</p>
+      {projectDetails.title? <p>{projectDetails.title}</p> : null}
+      {projectDetails.repoLink? <p>{projectDetails.repoLink}</p> : null}
+      {projectDetails.siteLink? <p>{projectDetails.siteLink}</p> : null}
     </section>
   )
 }
 
 ProjectsModal.propTypes = {
   visibility: PropTypes.string.isRequired,
-  handleOffHover: PropTypes.func.isRequired
+  handleOffHover: PropTypes.func.isRequired,
+  projectDetails: PropTypes.object.isRequired
 }
