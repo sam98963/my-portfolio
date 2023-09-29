a6607c73ba1ddf48ac86571cba9598635361280e
sam98963 <szwilliamson01@gmail.com>
2023-09-29 18:59:59 +0100

    attempts with fadeins with tailwind

--- src/Components/ProjectsModal/ProjectsModal.jsx
+++ src/Components/ProjectsModal/ProjectsModal.jsx
@@ -1,11 +1,14 @@
 import PropTypes from "prop-types"
 export default function ProjectsModal({visibility, handleOffHover, projectDetails}){
-    let modalClass = `fixed inset-0 flex items-center justify-center bg-red-500 w-full h-full z-20 ${visibility}`
+    let modalClass = `fixed inset-0 flex items-center justify-center bg-red-500 bg-opacity-90 w-full h-full z-20 ${visibility}`
+    let articleClass = `bg-red-500 bg-opacity-90 p-6 rounded-lg shadow-lg ${visibility === "block" ? "opacity-100" : "opacity-0"}`;
   return(
     <section onMouseLeave={handleOffHover} className={modalClass}>
-      {projectDetails.title? <p>{projectDetails.title}</p> : null}
-      {projectDetails.repoLink? <p>{projectDetails.repoLink}</p> : null}
-      {projectDetails.siteLink? <p>{projectDetails.siteLink}</p> : null}
+      <article className={articleClass}>
+        {projectDetails.title? <p>{projectDetails.title}</p> : null}
+        {projectDetails.repoLink? <p>{projectDetails.repoLink}</p> : null}
+        {projectDetails.siteLink? <p>{projectDetails.siteLink}</p> : null}
+      </article>
     </section>
   )
 }
