de87402a4d32a6de2670be7f5d1c19b113cb56d5
sam98963 <szwilliamson01@gmail.com>
2023-09-30 12:07:41 +0100

    responsive

--- src/Components/ProjectsModal/ProjectsModal.jsx
+++ src/Components/ProjectsModal/ProjectsModal.jsx
@@ -7,16 +7,16 @@ export default function ProjectsModal({visibility, handleOffHover, projectDetail
     let modalClass = `fixed inset-0 flex items-center justify-center bg-forth transition-fadeIn bg-opacity-90 w-full h-full z-20 ${visibility}`
   return(
     <section onMouseLeave={handleOffHover} className={modalClass}>
-      <article className="-rotate-24 flex flex-col">
-        {projectDetails.title? <p className="text-7xl border-b-4 border-black">{projectDetails.title}</p> : null}
-        <div className="flex flex-row w-4/5 self-end justify-end items-center">
+      <article className="-rotate-24 flex flex-col text-end">
+        {projectDetails.title? <p className="lg:text-7xl md:text-5xl sm:text-3xl text-2xl md:border-b-4 border-b-2 border-black">{projectDetails.title}</p> : null}
+        <div className="flex md:flex-row flex-col items-end justify-center w-full md:w-4/5 self-end md:justify-end md:items-center text-xs sm:text-sm md:text-md flex-wrap">
          
-         {projectDetails.repoLink? <p className="ml-4 underline cursor-pointer">
-          <a href={projectDetails.repoUrl} rel="noreferrer" target="_blank">{projectDetails.repoLink}<FontAwesomeIcon className="text-sm underline ml-1" icon={faArrowUpRightFromSquare} /></a>
+         {projectDetails.repoLink? <p className="ml-4 underline cursor-pointer flex flex-row">
+          <a href={projectDetails.repoUrl} rel="noreferrer" target="_blank">{projectDetails.repoLink}<FontAwesomeIcon className="text-2xs sm:text-xs md:text-sm underline ml-1" icon={faArrowUpRightFromSquare} /></a>
           </p> : null}
           
-          {projectDetails.siteLink? <p className="ml-4 underline cursor-pointer">
-          <a href={projectDetails.siteUrl} rel="noreferrer" target="_blank">{projectDetails.siteLink}<FontAwesomeIcon className="text-sm underline ml-1" icon={faArrowUpRightFromSquare} /></a>
+          {projectDetails.siteLink? <p className="ml-4 underline cursor-pointer flex flex-row">
+          <a href={projectDetails.siteUrl} rel="noreferrer" target="_blank">{projectDetails.siteLink}<FontAwesomeIcon className="text-2xs sm:text-xs md:text-sm underline ml-1" icon={faArrowUpRightFromSquare} /></a>
           </p> : null}
         </div>
       </article>
