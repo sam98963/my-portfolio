4f873a8d38da38dac49e2997bbbe5e8e1ef3053a
sam98963 <szwilliamson01@gmail.com>
2023-10-02 12:46:42 +0100

    hover effects on header links

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,36 @@
-export default function Header(){
-  return(
+export default function Header() {
+  return (
     <header className="h-20 sm:h-36 w-full flex items-center justify-between text-secondary">
-      <img />
+      <img alt="Logo" />
       <div className="h-full flex w-3/5 sm:w-2/5 items-end justify-around mb-10 lg:text-2xl sm:text-xl text-sm xs:text-md">
-        <p>Projects</p>
-        <p>About</p>
-        <p className="pr-2">Contact</p>
+        <p className="relative group">
+          <a
+            href="#"
+            className="hover:text-fifth transition-colors duration-300 ease-in-out"
+          >
+            Projects
+            <span className="absolute left-0 w-0 h-1 bg-fifth transition-width duration-300 ease-in-out group-hover:w-full"></span>
+          </a>
+        </p>
+        <p className="relative group">
+          <a
+            href="#"
+            className="hover:text-fifth transition-colors duration-300 ease-in-out"
+          >
+            About
+            <span className="absolute left-0 w-0 h-1 bg-fifth transition-width duration-300 ease-in-out group-hover:w-full"></span>
+          </a>
+        </p>
+        <p className="relative group">
+          <a
+            href="#"
+            className="hover:text-fifth transition-colors duration-300 ease-in-out"
+          >
+            Contact
+            <span className="absolute left-0 w-0 h-1 bg-fifth transition-width duration-300 ease-in-out group-hover:w-full"></span>
+          </a>
+        </p>
       </div>
     </header>
-  )
+  );
 }
