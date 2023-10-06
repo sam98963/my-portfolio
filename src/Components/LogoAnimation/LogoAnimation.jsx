8837e6c7252801012002de1675d70e50fe391401
sam98963 <szwilliamson01@gmail.com>
2023-10-06 16:10:47 +0100

    Logo is responsive

--- src/Components/LogoAnimation/LogoAnimation.jsx
+++ src/Components/LogoAnimation/LogoAnimation.jsx
@@ -1,16 +1,16 @@
 import "./LogoAnimation.css"
 export default function LogoAnimation(){
   return(
-    <div id = "avatar" className="mx-2 sm:mx-8 h-16 sm:h-24 w-16 sm:w-24 rounded-full border-2 border-white relative">
+    <div id = "avatar" className="mx-8 sm:mx-16 h-12 sm:h-24 w-12 sm:w-24 rounded-full border-2 border-white relative mt-6 sm:mt-12">
       <div id="goggles" className=" w-8 h-8 rounded-full">
         <div>
 
         </div>
       </div>
-      <div id="guitar" className=" w-8 h-8 rounded-full "></div>
-      <div id="football" className=" w-8 h-8 rounded-full"></div>
-      <div id="trumpet" className=" w-8 h-8 rounded-full"></div>
-      <div id="bike" className=" w-8 h-8 rounded-full"></div>
+      <div id="guitar" className=" sm:w-8 sm:h-8 w-6 h-6  rounded-full "></div>
+      <div id="football" className=" sm:w-8 sm:h-8 w-6 h-6 rounded-full"></div>
+      <div id="trumpet" className=" sm:w-8 sm:h-8 w-6 h-6 rounded-full"></div>
+      <div id="bike" className=" sm:w-8 sm:h-8 w-6 h-6 rounded-full"></div>
     </div>
   )
 }
