bec3dd123a5706b87abfed7cc9345549505d3d8b
sam98963 <szwilliamson01@gmail.com>
2023-10-06 10:55:37 +0100

    animation complete, need to replace images to the more solid ones, or add a bg ring - also need to adjust size of animation for screen sizes

--- src/Components/LogoAnimation/LogoAnimation.jsx
+++ src/Components/LogoAnimation/LogoAnimation.jsx
@@ -1,10 +1,12 @@
 import "./LogoAnimation.css"
 export default function LogoAnimation(){
   return(
-    <div className="mx-2 sm:mx-8 h-16 sm:h-24 w-16 sm:w-24 rounded-full border-2 border-white relative">
-      <div id="goggles" className=" w-6 h-6 rounded-full bg-white"></div>
-      <div id="guitar" className=" w-6 h-6 rounded-full bg-white"></div>
-      <div id="games" className=" w-6 h-6 rounded-full bg-white"></div>
+    <div id = "avatar" className="mx-2 sm:mx-8 h-16 sm:h-24 w-16 sm:w-24 rounded-full border-2 border-white relative">
+      <div id="goggles" className=" w-8 h-8 rounded-full"></div>
+      <div id="guitar" className=" w-8 h-8 rounded-full "></div>
+      <div id="football" className=" w-8 h-8 rounded-full"></div>
+      <div id="trumpet" className=" w-8 h-8 rounded-full"></div>
+      <div id="bike" className=" w-8 h-8 rounded-full"></div>
     </div>
   )
 }
