82909d3524382e9eed3acec00eb79da51d1e5552
sam98963 <szwilliamson01@gmail.com>
2023-10-09 16:13:01 +0100

    animation not quite sorted

--- src/Components/LogoAnimation/LogoAnimation.jsx
+++ src/Components/LogoAnimation/LogoAnimation.jsx
@@ -2,15 +2,20 @@ import "./LogoAnimation.css"
 export default function LogoAnimation(){
   return(
     <div id = "avatar" className="mx-8 sm:mx-20 h-64 md:h-72 lg:h-88 w-64 md:w-72 lg:w-88 rounded-full border-2 border-white relative my-16 sm:my-12 sm:mt-20 my">
-      <div id="goggles" className=" w-8 h-8 rounded-full">
-        <div>
-
-        </div>
-      </div>
-      <div id="guitar" className="rounded-full "></div>
-      <div id="football" className="rounded-full"></div>
-      <div id="trumpet" className="rounded-full"></div>
-      <div id="bike" className="rounded-full"></div>
+      <div id="html" className=" w-8 h-8 rounded-full"></div>
+      <div id="css" className="rounded-full "></div>
+      <div id="javascript" className="rounded-full"></div>
+      <div id="typescript" className="rounded-full"></div>
+      <div id="react" className="rounded-full"></div>
+      <div id="java" className="rounded-full"></div>
+      <div id="spring" className="rounded-full"></div>
+      <div id="express" className="rounded-full"></div>
+      <div id="postqresql" className="rounded-full"></div>
+      <div id="git" className="rounded-full"></div>
+      <div id="github" className="rounded-full"></div>
+      <div id="jest" className="rounded-full"></div>
+      <div id="mocha" className="rounded-full"></div>
+      <div id="figma" className="rounded-full"></div>
     </div>
   )
 }
