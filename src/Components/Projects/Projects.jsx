c56e17c5c4b4e0bec9c486fd7a0a09b09f8ebe63
sam98963 <szwilliamson01@gmail.com>
2023-10-10 17:00:37 +0100

    formatting nearly done

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,19 +1,19 @@
 export default function Projects(){
   return (
     <main id="projects" className="w-screen h-screen flex items-center justify-center bg-gradient-to-br from-primary via-forth to-tertiary">
-      <section className="w-11/12 h-5/6 bg-white flex flex-col items-center justify-around md:flex-row md:flex-wrap md:justify-around md:items-center">
-        <figure className="md:w-5/12 w-10/12 h-48 sm:h-56 md:h-108 bg-red-500">
+      <section className="w-12/13 h-7/8 flex flex-col items-center justify-around lg:flex-row lg:flex-wrap lg:justify-around lg:items-center">
+        <figure className="lg:w-144 md:w-120 md:w-88 w-5/6  h-48 sm:h-56 lg:h-120 bg-gray-100 opacity-80 my-2">
           
         </figure>
 
-        <figure className="md:w-5/12 w-10/12 h-48 sm:h-56 md:h-108 bg-red-500">
+        <figure className="lg:w-144 md:w-120 md:w-88 w-5/6 h-48 sm:h-56 lg:h-120  bg-gray-100 opacity-80 my-2">
           
         </figure>
-        <figure className="md:w-5/12 w-10/12 h-48 sm:h-56 md:h-108 bg-red-500">
+        <figure className="lg:w-144 md:w-120 md:w-88 w-5/6 h-48 sm:h-56 lg:h-120 bg-gray-100 opacity-80 my-2">
           
         </figure>
 
-        <figure className="md:w-5/12 w-10/12 h-48 sm:h-56 md:h-108 bg-red-500">
+        <figure className="lg:w-144 md:w-120 md:w-88 w-5/6 h-48 sm:h-56 lg:h-120 bg-gray-100  opacity-80 my-2">
           
         </figure>
       </section>
