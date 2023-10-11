2d02229ac9e34cf94080464b2503896f97629124
sam98963 <szwilliamson01@gmail.com>
2023-10-11 14:43:19 +0100

    button styling

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,42 +2,43 @@ import lyra from "../../assets/Lyra.png"
 import mmm from "../../assets/MMM.png"
 import portfolio from "../../assets/Portfolio.png"
 import three from "../../assets/ThreeRedesign.png"
+import "./Projects.css"
 
 
 export default function Projects(){
   return (
     <main id="projects" className="w-screen h-fit flex items-center justify-center bg-gradient-to-br from-primary via-forth to-tertiary">
-      <section className="w-full h-7/8 flex flex-col items-center justify-around lg:flex-row lg:flex-wrap lg:justify-around lg:items-center">
+      <section className="w-full h-7/8 flex flex-col items-center justify-around lg:flex-row lg:flex-wrap lg:justify-around lg:items-center p-5">
         
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={lyra} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
+            <button className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
+            <button className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={three} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
+            <button className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
+            <button className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={portfolio} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
+            <button className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
+            <button className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={mmm} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
-            <button className="btn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
+            <button className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Site</span></button>
+            <button className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center"><span>Repo</span></button>
           </div>
         </figure>
       </section>
