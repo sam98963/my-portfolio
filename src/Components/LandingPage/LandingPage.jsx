c96f32bf170f8523ef27dbf317ef49d0203ad548
sam98963 <szwilliamson01@gmail.com>
2023-10-25 17:03:46 +0100

    h-fit until large screens to prevent over spacing on tablets. changed portfolio image and resizing text on portfolio buttons

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import Header from "../Header/Header"
 import LogoAnimation from "../LogoAnimation/LogoAnimation"
 export default function LandingPage(){
   return(
-    <main className="h-fit md:h-screen w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
+    <main className="h-fit lg:h-screen w-screen bg-gradient-to-tr from-primary via-forth to-tertiary text-secondary flex flex-col">
       <Header />
       <section className="w-full h-full flex flex-col text-start sm:p-15 p-5 sm:p-10 md:pt-10">
         <article>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import lyra from "../../assets/Lyra.png"
 import mmm from "../../assets/MMM.png"
-import portfolio from "../../assets/Portfolio.png"
+import portfolio from "../../assets/Portfolio2.png"
 import three from "../../assets/ThreeRedesign.png"
 import "./Projects.css"
 
@@ -15,32 +15,32 @@ export default function Projects(){
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={three} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <a href="https://three-login-redesign.netlify.app/" rel="noreferrer" target="_blank" className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Site</span></a>
-            <a href="https://github.com/sam98963/Three_Login_Redesign" rel="noreferrer" target="_blank" className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Repo</span></a>
+            <a href="https://three-login-redesign.netlify.app/" rel="noreferrer" target="_blank" className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Site</span></a>
+            <a href="https://github.com/sam98963/Three_Login_Redesign" rel="noreferrer" target="_blank" className="three smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Repo</span></a>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={portfolio} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <a href="#" className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Site</span></a>
-            <a href="https://github.com/sam98963/my-portfolio" rel="noreferrer" target="_blank" className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Repo</span></a>
+            <a href="#" className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Site</span></a>
+            <a href="https://github.com/sam98963/my-portfolio" rel="noreferrer" target="_blank" className="portfolio smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Repo</span></a>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
           <img src={mmm} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <a href="https://mightyminiminds.netlify.app/" rel="noreferrer" target="_blank" className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Site</span></a>
-            <a href="https://github.com/sam98963/mighty_mini_minds" rel="noreferrer" target="_blank" className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>Repo</span></a>
+            <a href="https://mightyminiminds.netlify.app/" rel="noreferrer" target="_blank" className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Site</span></a>
+            <a href="https://github.com/sam98963/mighty_mini_minds" rel="noreferrer" target="_blank" className="mmm smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>Repo</span></a>
           </div>
         </figure>
 
         <figure className="lg:w-144 md:w-120 sm:w-88 w-5/6 h-48 sm:h-56 lg:h-120  my-2 flex flex-col items-center justify-between">
             <img src={lyra} className="h-7/8"/>
           <div className="w-4/5 h-fill flex justify-around p-2 lg:p-0">
-            <a className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>In Progress</span></a>
-            <a className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer"><span>In Progress</span></a>
+            <a className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>In Progress</span></a>
+            <a className="lyra smlbtn w-2/5 bg-white rounded-sm rounded-xl relative flex items-center justify-center cursor-pointer text-sm sm:text-base"><span>In Progress</span></a>
           </div>
         </figure>
       </section>
